963422031815b5801ca60e5b209c27fbdca8af95
SebastianChace <SebastianChace@10.50.65.10>
2016-01-12 16:16:49 -0300

    Se añaden traducciones al español y al inglés.

--- lang/en/evapares.php
+++ lang/en/evapares.php
@@ -1,20 +1,4 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-
 /**
  * English strings for evapares
  *
@@ -28,12 +12,36 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['addSSC'] = 'Add SSC' ;
+$string['answer'] = 'Answer ' ;
+$string['averagePeersEval'] = 'Peers Evaluation Average: ' ;
+$string['amountOfAnswers'] = 'Amount of answers available for each question.' ;
+$string['amountOfEvaluations'] = 'Amount of Partial evaluations (Leaving out the initial and final ones)' ;
+$string['amountOfQuestions'] = 'Amount of Questions to be answered in each evaluation' ;
+$string['disponibilityTime'] = 'Amount of Days to complete each evaluation.' ;
+$string['CalificationResults'] = 'Calification Results' ;
+$string['dueDate'] = 'Due Date: ' ;
+$string['DeliverableDetails'] = 'Deliverable Details' ;
+$string['DeliverableName'] = 'Deliverable Name ' ;
+$string['DeliverableDate'] = 'Due Date' ;
+$string['evaparesfieldset'] = 'Custom example fieldset';
+$string['evaparesname'] = 'Evaluations name';
+$string['evaparesname_help'] = 'You MUST name your evaluations. This name will be common to all of them';
+$string['evapares'] = 'evapares';
+$string['finalDate'] = 'Final Evaluation';
+$string['gradingStuden'] = 'Student to be graded ' ;
 $string['modulename'] = 'evapares';
 $string['modulenameplural'] = 'evaparess';
-$string['modulename_help'] = 'Use the evapares module for... | The evapares module allows...';
-$string['evaparesfieldset'] = 'Custom example fieldset';
-$string['evaparesname'] = 'evapares name';
-$string['evaparesname_help'] = 'This is the content of the help tooltip associated with the evaparesname field. Markdown syntax is supported.';
-$string['evapares'] = 'evapares';
+$string['modulename_help'] = 'Use the evapares module to create instances for studentes to 
+		evaluate themselves and their team member| The evapares module allows for students to
+		grade their peers at different stages of the project.';
+$string['noCapabilities']= "You don't have the capabilities to be here" ;
+$string['option'] = "Option" ;
+$string['peers_results'] = 'Peers Evaluation Result ' ;
+$string['personalEvalInitial'] = 'Initial Personal Evaluation' ;
+$string['personalEval'] = 'Personal Evaluation' ;
 $string['pluginadministration'] = 'evapares administration';
 $string['pluginname'] = 'evapares';
+$string['question']='Question' ;
+$string['ssc_results'] = 'SSC Evaluation Results' ;
+$string['ssc_help'] = 'The Stop, Start, Continue (SSC) methodogy consists of three text boxes. Inside the “Stop” box the student writes what he thinks his groupmate should stop doing. In the “Start” box the student writes what his groupmate should start doing. Finally, in the “Continue”, the student has the option to write what he sees as his groupmate’s best traits and what he should continue doing. This methodology is used for diverse motives, spanning communication, personal development, teamwork, problem solving, among others. ' ;
---
+++ lang/es/evapares.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * English strings for evapares
+*
+* You can have a rather longer description of the file as well,
+* if you like, and it can span multiple lines.
+*
+* @package    mod_evapares
+* @copyright  2015 Your Name
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addSSC'] = 'Agregar SSC' ;
+$string['answer'] = 'Respuesta ' ;
+$string['averagePeersEval'] = 'Promedio Calificación de Pares: ' ;
+$string['amountOfAnswers'] = 'Cantidad de Respuestas a Seleccionar en Cada Pregunta' ;
+$string['amountOfEvaluations'] = 'Cantidad de Entregas Parciales (Sin incluir entrega inicial y final)' ;
+$string['amountOfQuestions'] = 'Cantidad de Preguntas que se Responderán en las Evaluaciones' ;
+$string['disponibilityTime'] = 'Cantidad de Tiempo para realizar cada evaluacion (En Dias)' ;
+$string['CalificationResults'] = 'Resultados Calificación' ;
+$string['dueDate'] = 'Fecha de Entrega: ' ;
+$string['DeliverableDetails'] = 'Detalle de Entregas' ;
+$string['DeliverableName'] = 'Nombre Entrega ' ;
+$string['DeliverableDate'] = 'Fecha de Entrega' ;
+$string['evaparesfieldset'] = 'Custom example fieldset';
+$string['evaparesname'] = 'Nombre de la evaluación';
+$string['evaparesname_help'] = 'Debe cambiar el nombre de la evaluación. Este nombre será común a todas las instancia de la evaluación';
+$string['evapares'] = 'evapares';
+$string['finalDate'] = 'Evaluacion Final';
+$string['gradingStuden'] = 'Estudiante a ser Evaluado ' ;
+$string['modulename'] = 'evapares';
+$string['modulenameplural'] = 'evaparess';
+$string['modulename_help'] = 'Utilice el módulo evapares para generar instancias de evaluación personal y grupal para
+		cada alumno| La evaluación de  evapares permite que los alumnos puedan evalaur a
+		sus compañeros de grupo en distintas etapas del proyecto.';
+$string['noCapabilities']= "No tiene la capabilidad de estar aquí" ;
+$string['option'] = "Opción" ;
+$string['peers_results'] = 'Resultados Evaluación Pares' ;
+$string['personalEvalInitial'] = 'Evaluacion Personal Inicial' ;
+$string['personalEval'] = 'Evaluación Personal' ;
+$string['pluginadministration'] = 'Administración Evapares';
+$string['pluginname'] = 'evapares';
+$string['question']='Pregunta' ;
+$string['ssc_results'] = 'Resultados Evaluación SSC' ;
+$string['ssc_help'] = 'La metodología Stop, Start, Continue (SSC) consiste en tres cuadros de texto, dentro del cuadro “Stop”, dónde el estudiante siguiere qué cosas realizadas por su compañero cree que no son útiles o que debería dejar de hacer. En el cuadro “Start”, el estudiante indica qué cree que su compañero debería comenzar a realizar dentro del trabajo en equipo. Finalmente, en el cuadro “Continue”, el estudiante tiene la opción de indicar cuáles cree que son las "mejores prácticas" que está realizando su compañero y que debiese continuar haciendo. Esta metodología es usada para variados propósitos relacionados al desarrollo y la comunicación, incluyendo el crecimiento personal, trabajo en equipo, solución de problemas, entre otros. ' ;
