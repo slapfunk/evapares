dad7e1fbec75c844e6a8fd7a507faa7a71ee8037
beespinosa <beespinosa@alumnos.uai.cl>
2016-01-14 16:44:36 -0300

    comprobacion de grupos

--- results_tab.php
+++ results_tab.php
@@ -1,1 +1,8 @@
 <?php
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once('/forms/forms_alu.php');
+
+$action = optional_param("action", "view", PARAM_TEXT);
+$cmid = required_param('id', PARAM_INT);
