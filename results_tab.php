2336ec1d0ac96eee450cb22da45b68f7b5857d1f
beespinosa <beespinosa@alumnos.uai.cl>
2016-01-18 16:37:46 -0300

    demases

--- results_tab.php
+++ results_tab.php
@@ -1,8 +1,31 @@
 <?php
+
+/**
+ * The main evapares configuration form
+ *
+ * It uses the standard core Moodle formslib. For more info about them, please
+ * visit: http://docs.moodle.org/en/Development:lib/formslib.php
+ *
+ * @package    mod_evapares
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once($CFG->dirroot.'/course/moodleform_mod.php');
-require_once('/forms/forms_alu.php');
 
-// $action = optional_param("action", "view", PARAM_TEXT);
-// $cmid = required_param('id', PARAM_INT);
+$cmid = required_param('id', PARAM_INT);
+
+/**
+ * Module instance settings form
+ *
+ * @package    mod_evapares
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+echo "mira tus 	resultado aqui";
+	
+	
