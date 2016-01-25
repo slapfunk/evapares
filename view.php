3d7dc067cdb959d2d3aa4c470a8c71cb11e5b680
SebastianChace <SebastianChace@10.50.65.16>
2016-01-25 16:46:32 -0300

    Tabla de Profesor completada totalmente. Observación: Si no existen caracteres escritos en algun SSC, no arrojará ningun valor en la tabla.

--- view.php
+++ view.php
@@ -205,7 +205,104 @@ if(has_capability('mod/evapares:courseevaluations', $context) && $action == "add
 }
 elseif(has_capability('mod/evapares:courseevaluations', $context) && $action == "view"){
 	
-	echo"holi, usted es profe";
+	
+$bidimensional = array() ;
+$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
+
+for($i=0; $i<($evapares->total_iterations) ; $i)
+	{
+	$i++ ;
+	array_push($headings,'Evaluacion numero '.$i) ;
+	}
+	array_push($headings, 'Evaluacion Final') ;
+	
+//Count the amount of students in the course cd.id=2
+$NumberOfStudentsInCourse = $DB->get_record_sql('SELECT cr.SHORTNAME, cr.FULLNAME,
+      											 COUNT(ra.ID) AS enrolled
+												 FROM   `MDL_COURSE` cr
+				       							 JOIN `MDL_CONTEXT` ct
+						        				 ON ( ct.INSTANCEID = cr.ID )
+						      					 LEFT JOIN `MDL_ROLE_ASSIGNMENTS` ra
+              									 ON ( ra.CONTEXTID = ct.ID )
+												 WHERE  ct.CONTEXTLEVEL = 50
+												       AND ra.ROLEID = 5
+                                                       AND cr.id = 2
+												 GROUP  BY cr.SHORTNAME,
+												          cr.FULLNAME
+												 ORDER  BY `ENROLLED` ASC ') ;
+//get group_id, user_id and user_name, and the sums for stop, start, continue
+$SUPERQUERY = $DB->get_records_sql('SELECT u.id AS userid, g.id AS group_id, u.username AS USERname, SUM(length("ssc_stop")) AS SumaStop,  SUM(length(`ssc_start`)) AS SumaStart, SUM(length(`ssc_continue`)) AS SumaContinue
+FROM mdl_user u
+INNER JOIN mdl_role_assignments ra ON ra.userid = u.id
+INNER JOIN mdl_context ct ON ct.id = ra.contextid
+INNER JOIN mdl_course c ON c.id = ct.instanceid
+INNER JOIN mdl_role r ON r.id = ra.roleid
+INNER JOIN mdl_groups_members gm ON gm.userid = u.id
+INNER JOIN mdl_groups g ON g.id = gm.groupid
+INNER JOIN mdl_evapares_evaluations eval ON u.id= eval.alu_evaluado_id
+WHERE length(`ssc_stop`)
+IN (SELECT LENGTH(`ssc_stop`)
+FROM mdl_evapares_evaluations eval
+   WHERE `alu_evalua_id`!=`alu_evaluado_id`)
+GROUP BY userid');
+
+$resultados = $DB->get_records_sql('SELECT `iterations_id`,alu_evalua_id AS evaluador
+		,alu_evaluado_id AS Evaluado,`answers`, iter.id AS iteration
+FROM mdl_evapares_evaluations eval
+INNER JOIN mdl_evapares_iterations iter ON iter.id = eval.iterations_id
+WHERE iter.evapares_id ='.$cmid.'
+ORDER BY `alu_evalua_id`, `iterations_id`') ; 
+$StartDate = $DB->get_records_sql('SELECT eval.id, iter.`start_date` FROM mdl_evapares_evaluations eval 
+								INNER JOIN mdl_evapares_iterations iter ON iter.id = eval.iterations_id 
+								GROUP BY iter.id') ; 
+$actualDate = time() ;
+
+//Table Headers
+$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
+//Add a column for every extra evaluation besides Initial and Final Ones
+for($i=0; $i<($evapares->total_iterations) ; $i){
+	$i++ ;
+	array_push($headings,'Ev. '.$i) ;
+}
+array_push($headings, 'Ev.Final') ;
+//Table Data
+foreach($SUPERQUERY AS $values)
+{
+	$bidimensional[$values->userid][0] =$values->group_id;
+	$bidimensional[$values->userid][1] =$values->username;
+	$bidimensional[$values->userid][2] =$values->userid;
+	$bidimensional[$values->userid][3] =$values->sumastop;
+	$bidimensional[$values->userid][4] =$values->sumastart;
+	$bidimensional[$values->userid][5] =$values->sumacontinue;
+	$partialKey = 1 ; 
+	foreach ($resultados AS $partialEvaluationsValues)
+	{
+		if($StartDate<= $actualDate)
+		{
+		if($partialEvaluationsValues->answers != 0)
+			{
+			$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/respondible.jpg" style=width:15px;height:15px;>';
+			}
+			else
+				{
+				$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/norespondible.jpg" style=width:15px;height:15px;>';
+				}
+		}
+		else
+		{
+		$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/norespondido.jpg" style=width:15px;height:15px;>';
+		}
+		$partialKey++ ;
+	}
+}
+
+
+echo "SUPERDUPER"; 
+$table = new html_table();
+$table->head = $headings ;
+$table->data = $bidimensional ;
+
+echo html_writer::table($table);
 }
 
 elseif(has_capability('mod/evapares:myevaluations', $context) && $action == "view"){
--- view_teacher.php
+++ view_teacher.php
@@ -55,91 +55,36 @@ $TotalLength = $DB->get_record_sql('SELECT u.username, sum(length("ssc_stop")) A
 									GROUP BY alu_evaluado_id') ;
 }
 
-//WE MUST DO A NEW MASSIVE QUERY
 
 
-
-
-foreach($NameOfTheStudentsInCourse->u.id AS $key=>$value)
-{
-$row = array() ;
-array_push($row, $value) ;
-$length = $TotalLength->SumaStop['$value'] ;
-array_push($bidimensional[$key], $row) ;
-
-
-
-
-}
 //Start of Table coding
-$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
-
-for($i=0; $i<($evapares->total_iterations) ; $i){
-	$i++ ;
-	array_push($headings,'Evaluacion numero '.$i) ;
-}
-
-array_push($headings, 'Evaluacion Final') ;
 
 $bidimensional = array() ;
+$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
 
-for($l=0 ; $l < $NumberOfStudentsInCourse; $l++){
-	//Queries for the answers written in those files
-	$alpha = $DB->get_record_sql('SELECT ssc_stop FROM mdl_evapares_evaluations WHERE id='.$l) ;
-	$beta = $DB->get_record_sql('SELECT ssc_start FROM mdl_evapares_evaluations WHERE id='.$l) ;
-	$gamma = $DB->get_record_sql('SELECT ssc_continue FROM mdl_evapares_evaluations WHERE id='.$l) ;
-	$name = $DB->get_record_sql ('SELECT id FROM mdl_groups WHERE id =1') ;
-	var_dump($name) ;
-	//LENGTH OF THE ANSWER  (Trim function could delete white spaces, preventing cheats)
-	$stop = strlen($alpha->text)  ;
-	$start = strlen($beta->text) ;
-	$continue = strlen($gamma->text) ;
-
-	//Here we build the bidimensional array to be displayed in each row
-	$bidimensional[$l]=array($stop,$start,$continue) ;
-
-}
-
-echo "<br> <br> <br>" ;
-var_dump($evapares->total_iterations) ;
-$table = new html_table();
-$table->head = $headings ;
-$table->data = 	//Codigo para la tabla
-	$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
-	
-	for($i=0; $i<($evapares->total_iterations) ; $i){
-		$i++ ;
-		array_push($headings,'Evaluacion numero '.$i) ;
+for($i=0; $i<($evapares->total_iterations) ; $i)
+	{
+	$i++ ;
+	array_push($headings,'Evaluacion numero '.$i) ;
 	}
-	
 	array_push($headings, 'Evaluacion Final') ;
 	
-	$bidimensional = array() ;
-	
-	for($l=0 ; $l < $cantidadDeAlumnos; $l++){
-		//Queries for the answers written in those files
-		$stop = $DB->get_record_sql("SELECT length(`ssc_stop`) FROM mdl_evapares_evaluations WHERE id=".$l) ;
-		$start = $DB->get_record_sql('SELECT ssc_start FROM mdl_evapares_evaluations WHERE id='.$l) ;
-		$continue = $DB->get_record_sql('SELECT ssc_continue FROM mdl_evapares_evaluations WHERE id='.$l) ;
-		$name = $DB->get_record_sql ('SELECT id FROM mdl_groups WHERE id =1') ; 
-		//LENGTH OF THE ANSWER  (Trim function could delete white spaces, preventing cheats)
-		$stopLength = strlen($stop->text)  ;
-		$startLength = strlen($start->text) ;
-		$continueLength = strlen($continue->text) ;
-	
-	
-		//Here we build the bidimensional array to be displayed in each row
-		$bidimensional[$l]=array($name,$stopLength,$startLength,$continueLength) ;
-	
-	}
-	get_enrolled_students($context,$withcapability = 'view',$groupid=2) ;
-	echo "<br> <br> <br>" ;
-	$table = new html_table();
-	$table->head = $headings ;
-	$table->data = $bidimensional ;
-	//Actual Table generating code below, do NOT tweak
-	
-	$SUPERQUERY = $DB->get_records_sql('SELECT g.id AS group_id, u.username AS USERname,u.id AS USERid, SUM(length("ssc_stop")) AS SumaStop,  SUM(length(`ssc_start`)) AS SumaStart, SUM(length(`ssc_continue`)) AS SumaContinue
+//Count the amount of students in the course cd.id=2
+$NumberOfStudentsInCourse = $DB->get_record_sql('SELECT cr.SHORTNAME, cr.FULLNAME,
+      											 COUNT(ra.ID) AS enrolled
+												 FROM   `MDL_COURSE` cr
+				       							 JOIN `MDL_CONTEXT` ct
+						        				 ON ( ct.INSTANCEID = cr.ID )
+						      					 LEFT JOIN `MDL_ROLE_ASSIGNMENTS` ra
+              									 ON ( ra.CONTEXTID = ct.ID )
+												 WHERE  ct.CONTEXTLEVEL = 50
+												       AND ra.ROLEID = 5
+                                                       AND cr.id = 2
+												 GROUP  BY cr.SHORTNAME,
+												          cr.FULLNAME
+												 ORDER  BY `ENROLLED` ASC ') ;
+//get group_id, user_id and user_name, and the sums for stop, start, continue
+$SUPERQUERY = $DB->get_records_sql('SELECT g.id AS group_id, u.username AS USERname,u.id AS USERid, SUM(length("ssc_stop")) AS SumaStop,  SUM(length(`ssc_start`)) AS SumaStart, SUM(length(`ssc_continue`)) AS SumaContinue
 FROM mdl_user u
 INNER JOIN mdl_role_assignments ra ON ra.userid = u.id
 INNER JOIN mdl_context ct ON ct.id = ra.contextid
@@ -153,34 +98,61 @@ IN (SELECT LENGTH(`ssc_stop`)
 FROM mdl_evapares_evaluations eval
    WHERE `alu_evalua_id`!=`alu_evaluado_id`)
 GROUP BY eval.alu_evaluado_id');
-	//Table Headers
-	$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
-	//Add a column for every extra evaluation besides Initial and Final Ones
-	for($i=0; $i<($evapares->total_iterations) ; $i){
-		$i++ ;
-		array_push($headings,'Ev. '.$i) ;
+
+$resultados = $DB->get_records_sql('SELECT `iterations_id`,alu_evalua_id AS evaluador
+		,alu_evaluado_id AS Evaluado,`answers`, iter.id AS iteration
+FROM mdl_evapares_evaluations eval
+INNER JOIN mdl_evapares_iterations iter ON iter.id = eval.iterations_id
+WHERE iter.evapares_id ='.$cmid.'
+ORDER BY `alu_evalua_id`, `iterations_id`') ; 
+$StartDate = $DB->get_records_sql('SELECT eval.id, iter.`start_date` FROM mdl_evapares_evaluations eval 
+								INNER JOIN mdl_evapares_iterations iter ON iter.id = eval.iterations_id 
+								GROUP BY iter.id') ; 
+$actualDate = time() ;
+
+//Table Headers
+$headings= array('Grupo', 'Integrante', 'Res','S','S','C','Ev. Parcial','Ev. Inicial');
+//Add a column for every extra evaluation besides Initial and Final Ones
+for($i=0; $i<($evapares->total_iterations) ; $i){
+	$i++ ;
+	array_push($headings,'Ev. '.$i) ;
+}
+array_push($headings, 'Ev.Final') ;
+//Table Data
+foreach($SUPERQUERY AS $values)
+{
+	$bidimensional[$values->userid][0] =$values->group_id;
+	$bidimensional[$values->userid][1] =$values->username;
+	$bidimensional[$values->userid][2] =$values->userid;
+	$bidimensional[$values->userid][3] =$values->sumastop;
+	$bidimensional[$values->userid][4] =$values->sumastart;
+	$bidimensional[$values->userid][5] =$values->sumacontinue;
+	$partialKey = 1 ; 
+	foreach ($resultados AS $partialEvaluationsValues)
+	{
+		if($StartDate<= $actualDate)
+		{
+		if($partialEvaluationsValues->answers != 0)
+			{
+			$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/respondible.jpg" style=width:15px;height:15px;>';
+			}
+			else
+				{
+				$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/norespondible.jpg" style=width:15px;height:15px;>';
+				}
+		}
+		else
+		{
+		$bidimensional[$values->userid][5+$partialKey] ='<img src="pix/norespondido.jpg" style=width:15px;height:15px;>';
+		}
+		$partialKey++ ;
 	}
-	
-	array_push($headings, 'Ev.Final') ;
-	//Table Data
-	
-	$bidimensional = array() ;
-	
-	foreach($SUPERQUERY AS $values) {
-		$bidimensional[$values->userid][0] =$values->group_id;
-		$bidimensional[$values->userid][1] =$values->username;
-		$bidimensional[$values->userid][2] =$values->userid;
-		$bidimensional[$values->userid][3] =$values->sumastop;
-		$bidimensional[$values->userid][4] =$values->sumastart;
-		$bidimensional[$values->userid][5] =$values->sumacontinue;
-	}
-	var_dump($bidimensional) ;
-	
-	
-echo "hola" ;
+}
+
+
+echo "SUPERDUPER"; 
+$table = new html_table();
+$table->head = $headings ;
+$table->data = $bidimensional ;
 
 echo html_writer::table($table);
-
-var_dump($CFG) ;
-
-
