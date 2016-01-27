44f43a6bf72e6733b1a1774ef3327dd306c8719a
beespinosa <beespinosa@alumnos.uai.cl>
2016-01-27 11:41:21 -0300

    evlalutons

--- results_tab.php
+++ results_tab.php
@@ -19,6 +19,10 @@ require_once($CFG->dirroot.'/course/moodleform_mod.php');
 
 global $CFG, $DB, $OUTPUT, $USER;
 
+$PAGE->requires->jquery ();
+$PAGE->requires->jquery_plugin ( 'ui' );
+$PAGE->requires->jquery_plugin ( 'ui-css' );
+
 $iterations = $DB->get_records("evapares_iterations", array('evapares_id'=>$cmid));
 
 $resultados = $DB->get_records("evapares_evaluations", array('alu_evaluado_id'=>$USER->id),'iterations_id ASC');
@@ -28,7 +32,7 @@ $query = "SELECT Q.text AS preg, Q.id AS pregid, A.text AS resp, A.id AS ansid
 		  WHERE Q.evapares_id = ? AND Q.id = A.question_id";
 
 $percentages = "SELECT Answer.`answers_id`, Evaluation.`iterations_id`
-				FROM mdl_evapares_evlalutons_has_answ Answer
+				FROM mdl_evapares_eval_has_answ Answer
 				JOIN mdl_evapares_evaluations Evaluation ON (Evaluation.id = Answer.evaluations_id )
 				JOIN mdl_evapares_iterations Iteration ON (Evaluation.`iterations_id` = Iteration.`id`)
 				WHERE Iteration.evapares_id = ?
@@ -69,8 +73,21 @@ foreach($resultados as $param){
  			if($n_table != 0){
  				
  				$table->data = $supa_data_sama;
- 				//echo $param->iterations_id - 1;
- 				echo '<strong>'.$iterations[$param->iterations_id - 1]->evaluation_name.'</strong><br>'; 				
+ 				
+ 				echo '<div class="panel-group" id="accordion" role="tablist"
+ 						aria-multiselectable="true">
+ 						<div class="panel panel-default">
+ 						<div role="tab" id="'.$iterations[$param->iterations_id - 1]->evaluation_name.'">
+ 						<h3 class="panel-title">
+ 						<a class="list-group-item " role="button" data-toggle="collapse"
+ 								data-parent="#accordion" href="#'.$iterations[$param->iterations_id - 1]->evaluation_name.'"
+ 										aria-controls="'.$iterations[$param->iterations_id - 1]->evaluation_name.'">
+ 												'.$iterations[$param->iterations_id - 1]->evaluation_name.'
+ 										</a>
+ 										</h3>
+ 										</div>';
+ 											
+ 				//echo '<strong>'.$iterations[$param->iterations_id - 1]->evaluation_name.'</strong><br>'; 				
  				//COMPROBAR CON FECHA
  				echo html_writer::table($table);
 
@@ -99,8 +116,18 @@ foreach($resultados as $param){
  						echo'</td></tr>';
  				}
  						echo '</table><hr>';
+ 						echo'</ul>
+					</div>
+				</div>
+			</div>
+ 						';
 			}
- 				
+			echo '<div id="'.$iterations[$param->iterations_id - 1]->evaluation_name.'" class="panel-collapse collapse in"
+					role="tabpanel" aria-labelledby="'.$iterations[$param->iterations_id - 1]->evaluation_name.'">
+					<div>
+					<ul class="list-group">';
+						
+			
  			$table = new html_table();
  			$table->head = $headings ;
  			$supa_data_sama=array();
